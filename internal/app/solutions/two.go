01e1d668aa03194c7171d2cf236ff14bc25d12ac
Jacob Patterson <jacob@jacobpa.com>
2021-12-05 13:09:57 -0500

    Add day 2 solutions

---
+++ internal/app/solutions/two.go
@@ -0,0 +1,31 @@
+package solutions
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func DayTwo(input []string) (int, error) {
+	x, y := 0, 0
+
+	for _, s := range input {
+		data := strings.Split(s, " ")
+		dir := data[0]
+		delta, _ := strconv.Atoi(data[1])
+
+		switch dir {
+		case "forward":
+			x += delta
+		case "up":
+			y += delta
+		case "down":
+			y -= delta
+		default:
+			return 0, errors.New(fmt.Sprintf("Unrecognized direction %v", dir))
+		}
+	}
+
+	return x * y, nil
+}
---
+++ internal/app/solutions/two_test.go
@@ -0,0 +1,49 @@
+package solutions
+
+import "testing"
+
+func TestDayTwoEmptyInput(t *testing.T) {
+	var input []string
+	expected := 0
+	actual, _ := DayTwo(input)
+	if expected != actual {
+		t.Fatalf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestDayTwoForwardOnly(t *testing.T) {
+	input := []string{"forward 10"}
+	expected := 0
+	actual, _ := DayTwo(input)
+	if expected != actual {
+		t.Fatalf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestDayTwoForwardAndUp(t *testing.T) {
+	input := []string{"forward 10", "up 10"}
+	expected := 100
+	actual, _ := DayTwo(input)
+	if expected != actual {
+		t.Fatalf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestDayTwoInvalidDirection(t *testing.T) {
+	input := []string{"forward 10", "back 10"}
+	expected := 0
+	actual, err := DayTwo(input)
+	if expected != actual {
+		t.Fatalf("Expected %v, got %v", expected, actual)
+	}
+	if err == nil {
+		t.Fatal("Expected error, got nil")
+	}
+}
+
+func BenchmarkDayTwo(b *testing.B) {
+	input := []string{"forward 10", "up 10", "down 10", "up 5", "forward 1"}
+	for n := 0; n < b.N; n++ {
+		DayTwo(input)
+	}
+}
